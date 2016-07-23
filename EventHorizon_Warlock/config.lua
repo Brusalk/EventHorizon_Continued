a464e1ff3f0e36265a671e201ad14084ecbde9b9
Brusalk <Brusalk@users.noreply.github.com>
2016-07-23 00:36:53 -0700

    Fixed cata talent req for real this time. Now only show DG/Infernal if GoSUP isn't talented.

--- EventHorizon_Warlock/config.lua
+++ EventHorizon_Warlock/config.lua
@@ -106,7 +106,7 @@ function EventHorizon:InitializeClass()
         requiredTalent = 20,
     })
     
-
+    
     -- *** Destruction *** --
     --Immolate
     self:newSpell({
@@ -125,7 +125,7 @@ function EventHorizon:InitializeClass()
         icon = 17962,
         requiredTree = 3,
     })
-    
+
     -- Chaos Bolt/Eradication
     self:newSpell({
         debuff = 196414,
@@ -141,7 +141,7 @@ function EventHorizon:InitializeClass()
         requiredTree = 3,
         requiredTalent = 20,
     })
-    
+
     --Havoc
     self:newSpell({
         debuff = {80240, 0},
@@ -155,13 +155,13 @@ function EventHorizon:InitializeClass()
         requiredTree = 3,
         requiredTalent = 6,
     })
-    
+
     -- Cata
     self:newSpell({
         cast = 152108,
         cooldown = 152108,
         requiredTree = 3,
-        requiredTalent = 6,
+        requiredTalent = 5,
     })
     
     -- Soul Harvest
@@ -178,11 +178,12 @@ function EventHorizon:InitializeClass()
         requiredTree = {1, 2, 3},
         requiredTalent = 17,
     })
-    
+
     -- Summon Doomguard/Infernal
     self:newSpell({
         cooldown = {18540, 1122},
+        requiredTalentUnselected = 16, -- Don't show if GoSup
         requiredTree = {1, 2, 3},
     })
-
+    
 end
