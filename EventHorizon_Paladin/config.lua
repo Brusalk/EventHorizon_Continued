c47ce7ebae208bec96a5ebfa3a4d260a997dee94
James <cluuey+github@gmail.com>
2017-08-29 21:01:08 +1000

    Paladin general update.
    New config provided by @Leylaa via Discord.
    
    Note. I don't have a paladin so it's untested by me.

--- EventHorizon_Paladin/config.lua
+++ EventHorizon_Paladin/config.lua
@@ -3,16 +3,25 @@ local usemouseover = true -- Make this false or nil (or just delete the line alt
 function EventHorizon:InitializeClass()
   self.config.gcdSpellID = 19750 -- Flash of Light
   self.config.hastedSpellID = {7328,10} -- Redemption, probably not needed at all
-  
 
+
+  -- ---------------------------------------
   -- Holy
+  -- ---------------------------------------
 
+  -- Crusader Strike
+   self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 3,
+    cooldown = 35395,
+  })
+  
   -- Judgment
   self:newSpell({
-    requiredTree = 2,
-    requiredLevel = 3,
+    requiredTree = 1,
+    requiredTalent = 18,
     cooldown = 20271,
-    debuff = 214222,
+    debuff = 196941,
   })
 
   -- Holy Shock
@@ -20,16 +29,26 @@ function EventHorizon:InitializeClass()
     requiredTree = 1,
     requiredLevel = 10,
     cooldown = 20473,
-    cast = {19750, 82326},
+	keepIcon = true,
+    cast = {20473, 19750, 82326},
   })
 
   -- Light of Dawn
   self:newSpell({
     requiredTree = 1,
-    requiredLevel = 58,
+    requiredLevel = 40,
     cooldown = 85222,
   })
 
+  -- Tyr's Deliverance
+  self:newSpell({
+    requiredTree = 1,
+    requiredArtifactTalent = 200652,
+    cooldown = 200652,
+    playerbuff = 200652,
+    auraunit = usemouseover and 'mouseover' or 'target',
+  })
+ 
   -- Bestow Faith
   self:newSpell({
     requiredTree = 1,
@@ -45,28 +64,19 @@ function EventHorizon:InitializeClass()
     playerbuff = 200025,
     auraunit = usemouseover and 'mouseover' or 'target',
   })
-
+  
   -- Holy Prism
   self:newSpell({
     requiredTree = 1,
     requiredTalent = 15,
     cooldown = 114165,
   })
-
+ 
   -- Light's Hammer
   self:newSpell({
     requiredTree = 1,
     requiredTalent = 2,
     cooldown = 114158,
-  })
-
-  -- Tyr's Deliverance
-  self:newSpell({
-    requiredTree = 1,
-    requiredArtifactTalent = 200652,
-    cooldown = 200652,
-    playerbuff = 200652,
-    auraunit = usemouseover and 'mouseover' or 'target',
   })
 
   -- Holy Avenger
@@ -76,124 +86,166 @@ function EventHorizon:InitializeClass()
     cooldown = 105809,
     playerbuff = 105809,
   })
-
+  
   -- Avenging Wrath
   self:newSpell({
     requiredTree = 1,
+	requiredLevel = 80,
     cooldown = 31842,
     playerbuff = 31842,
   })
-
-  -- Aura of Mercy
-  self:newSpell({
-    requiredTree = 1,
-    requiredTalent = 12,
-    cooldown = 183415,
-  })
-
+  
   -- Aura Mastery
   self:newSpell({
     requiredTree = 1,
-    requiredLevel = 65,
+    requiredLevel = 70,
     cooldown = 31821,
     playerbuff = 31821,
   })
-
-
+    
+  -- Rule of Law
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 6,
+	playerbuff = 214202,
+    recharge = 214202,
+  })
+  
+ 
+  -- ---------------------------------------
   -- Protection
+  -- ---------------------------------------
 
-  -- Judgment
+
+  -- Hammer of the Righteous
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 10,
+	requiredTalentUnselected = 2,
+	recharge = 53595,
+  })
+  
+  -- Blessed Hammer
+  self:newSpell({
+    requiredTree = 2,
+	requiredTalent = 2,
+    -- debuff = 204301,
+	recharge = 204019,
+  })
+
+    -- Judgment
   self:newSpell({
     requiredTree = 2,
     requiredLevel = 3,
-    cooldown = 20271,
-    debuff = 196941,
+    --cooldown = 20271,
+	recharge = 20271,
   })
 
   -- Consecration
   self:newSpell({
     requiredTree = 2,
-    requiredLevel = 32,
+    requiredLevel = 14,
     cooldown = 26573,
-    debuff = {26573,1},
+    --debuff = {26573,1},
   })
-
-  -- Hammer of the Righteous/Blessed Hammer
-  self:newSpell({
-    requiredTree = 2,
-    requiredLevel = 10,
-    cooldown = {53595, 204019},
-    debuff = 204301,
-  })
-
+  
   -- Avenger's Shield
   self:newSpell({
     requiredTree = 2,
     requiredLevel = 10,
     cooldown = 31935,
   })
-
-  -- Shield of the Righteous & Seraphim
+  
+  -- Shield of the Righteous
   self:newSpell({
     requiredTree = 2,
-    requiredLevel = 24,
-    cooldown = {53600, 152262},
-    playerbuff = {53600, 152262},
+    requiredLevel = 20,
+	playerbuff = 53600,
+    recharge = 53600,
   })
 
+    -- Eye of Tyr
+  self:newSpell({
+    requiredTree = 2,
+	requiredArtifactTalent = 209202,
+    cooldown = 209202,
+    debuff = 209202,
+  })
+
+    -- Light of the Protector
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 40,
+	requiredTalentUnselected = 13,
+    cooldown = 184092,
+  })
+  
+  -- Hand of the Protector
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 13,
+    cooldown = 213652,
+  })
+  
+--[[
   -- Knight Templar
   self:newSpell({
     requiredTree = 2,
     requiredTalent = 14,
-    cooldown = 204139,
-    playerbuff = 204139,
+    playerbuff = 221886,
+	recharge = 190784,
   })
+]]--
 
-  -- Light of the Protector/Hand of the Protector
+    -- Seraphim
   self:newSpell({
     requiredTree = 2,
-    requiredLevel = 12,
-    cooldown = {184092, 213652},
-  })
-
-  -- Eye of Tyr
-  self:newSpell({
-    requiredTree = 2,
-    requiredLevel = 100,
-    cooldown = 209202,
-    debuff = 209202,
+    requiredTalent = 20,
+    cooldown = 152262,
+    playerbuff = 152262,
   })
 
   -- Avenging Wrath
   self:newSpell({
     requiredTree = 2,
+	requiredLevel = 80,
     cooldown = 31884,
     playerbuff = 31884,
   })
 
   -- Bastion of Light
   self:newSpell({
-    requiredTree = 2,
+	requiredTree = 2,
+    requiredTalent = 5,
     cooldown = 204035,
   })
 
+--[[
   -- Blessing of Spellwarding
   self:newSpell({
     requiredTree = 2,
+	requiredTalent = 10,
     cooldown = 204018,
     playerbuff = 204018,
     auraunit = usemouseover and 'mouseover' or 'player',
   })
+]]--
 
+--[[
   -- Aegis of Light
   self:newSpell({
     requiredTree = 2,
+	requiredTalent = 16,
     cooldown = 204150,
     playerbuff = 204150,
     auraunit = usemouseover and 'mouseover' or 'player',
   })
+]]--
 
+
+  -- ---------------------------------------
   -- Retribution
+  -- ---------------------------------------
 
   -- Judgment
   self:newSpell({
@@ -203,36 +255,12 @@ function EventHorizon:InitializeClass()
     debuff = {197277,0},
   })
 
-  -- Crusader Strike
-  self:newSpell({
-    requiredTree = 3,
-    requiredLevel = 1,
-    requiredTalentUnselected = 5,
-    cooldown = 35395,
-  })
-
-  -- Zeal
-  self:newSpell({
-    requiredTree = 3,
-    requiredTalent = 5,
-    cooldown = 217020,
-    debuff = {217020,0},
-  })
-
   -- Blade of Justice
   self:newSpell({
     requiredTree = 3,
-    requiredLevel = 16,
-    requiredTalentUnselected = 11,
+    requiredLevel = 20,
+    requiredTalentUnselected = 12,
     cooldown = 184575,
-  })
-
-  -- Blade of Wrath
-  self:newSpell({
-    requiredTree = 3,
-    requiredTalent = 11,
-    cooldown = 202270,
-    debuff = {202270,2},
   })
 
   -- Divine Hammer
@@ -243,13 +271,36 @@ function EventHorizon:InitializeClass()
     debuff = {198034,2},
   })
 
+  -- Wake of Ashes
+  self:newSpell({
+    requiredTree = 3,
+    requiredArtifactTalent = 205273,
+    cooldown = 205273,
+    debuff = 205273,
+  })
+  
   -- Execution Sentence
   self:newSpell({
     requiredTree = 3,
     requiredTalent = 2,
     cooldown = 213757,
     debuff = {213757,7},
-    hasted = true,
+    --hasted = true,
+  })
+ 
+    -- Crusader Strike
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalentUnselected = 5,
+    cooldown = 35395,
+  })
+
+  -- Zeal
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 5,
+    cooldown = 217020,
+    --playerbuff = 217020,
   })
 
   -- Consecration
@@ -259,25 +310,17 @@ function EventHorizon:InitializeClass()
     cooldown = 205228,
   })
 
-  -- Wake of Ashes
+  -- Holy Wrath
   self:newSpell({
     requiredTree = 3,
-    requiredArtifactTalent = 205273,
-    cooldown = 205273,
-    debuff = 205273,
-  })
-
-  -- Eye for an Eye
-  self:newSpell({
-    requiredTree = 3,
-    requiredTalent = 14,
-    cooldown = 205191,
-    playerbuff = 205191,
+    requiredTalent = 21,
+    cooldown = 210220,
   })
 
   -- Avenging Wrath
   self:newSpell({
     requiredTree = 3,
+	requiredLevel = 80,
     requiredTalentUnselected = 20,
     cooldown = 31884,
     playerbuff = 31884,
@@ -287,24 +330,13 @@ function EventHorizon:InitializeClass()
   self:newSpell({
     requiredTree = 3,
     requiredTalent = 20,
-    cooldown = 224668,
-    playerbuff = 224668,
+    cooldown = 231895,
+    playerbuff = 231895,
   })
 
-  -- Shield of Vengeance
-  self:newSpell({
-    requiredTree = 3,
-    requiredTalent = 9,
-    cooldown = 115750,
-    playerbuff = 115750,
-  })
 
-  -- Holy Wrath
-  self:newSpell({
-    requiredTree = 3,
-    requiredTalent = 21,
-    cooldown = 210220,
-  })
+  -- Retribution Survivability Abilities and Talents
+
 
   -- Divine Shield
   self:newSpell({
@@ -313,6 +345,27 @@ function EventHorizon:InitializeClass()
     cooldown = 642,
     playerbuff = 642,
   })
+
+--[[
+  -- Shield of Vengeance
+  self:newSpell({
+    requiredTree = 3,
+    cooldown = 184662,
+    playerbuff = 184662,
+  })
+]]--
+
+--[[
+  -- Eye for an Eye
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 14,
+    cooldown = 205191,
+    playerbuff = 205191,
+  })
+]]--
+
+
 
   -- Situational CC spells, not included by default.
 --[[
