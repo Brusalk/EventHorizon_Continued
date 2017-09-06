f5423830cf0fb845141da8d13900a4a0ecc08297
James <cluuey+github@gmail.com>
2017-09-06 15:20:32 +1000

    Deathknight update for 7.3 patch. (#62)
    * Deathknight update for 7.3 patch.
    
    Updated to reflect the talent changes.
    Tweaked some bar colours.
    
    Feedback most welcome.
    My DK is an alt and I mostly play him Unholy, lots of Frost changes this patch and I'm not familiar enough to knock if this config works well enough.
    
    * Deathknight config for 7.3 patch, update.
    
    Changed tabs to spaces for consistent formatting.

--- EventHorizon_Deathknight/config.lua
+++ EventHorizon_Deathknight/config.lua
@@ -9,9 +9,9 @@ function EventHorizon:InitializeClass()
     requiredLevel = 10,
     playerbuff = 77535,
     barcolors = {
-      cooldown    = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {252/255, 010/255, 020/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {252/255, 010/255, 020/255, 0.7}
+     },
   })
 
   -- Bone Shield buff & Consumption (artifact ability) CD
@@ -22,9 +22,9 @@ function EventHorizon:InitializeClass()
     cooldown = 205223,
     smallCooldown = true,
     barcolors = {
-      cooldown    = {202/255, 161/255, 050/255, 0.7},
-      playerbuff  = {171/255, 202/255, 111/255, 0.3}
-    },
+     cooldown    = {202/255, 161/255, 050/255, 0.7},
+     playerbuff  = {171/255, 202/255, 111/255, 0.3}
+     },
   })
 
   -- Blood Plague & Blood Boil
@@ -34,9 +34,9 @@ function EventHorizon:InitializeClass()
     debuff = {55078,3},
     recharge = 50842,
     barcolors = {
-      recharge    = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {111/255, 010/255, 191/255, 0.6}
-    },
+     recharge    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {111/255, 010/255, 191/255, 0.6}
+     },
   })
 
   -- Death and Decay & Crimson Scourge
@@ -47,9 +47,9 @@ function EventHorizon:InitializeClass()
     smallCooldown = true,
     playerbuff = 81136,
     barcolors = {
-      cooldown    = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {171/255, 030/255, 010/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {171/255, 030/255, 010/255, 0.7}
+     },
   })
 
   -- Blooddrinker
@@ -75,9 +75,9 @@ function EventHorizon:InitializeClass()
     cooldown = 206977,
     debuff = 206977,
     barcolors = {
-      cooldown   = {171/255, 191/255, 181/255, 0.6},
-      debuffmine = {232/255, 090/255, 171/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {232/255, 090/255, 171/255, 0.7}
+     },
   })
 
   -- Mark of Blood
@@ -112,9 +112,9 @@ function EventHorizon:InitializeClass()
     cooldown = 48707,
     playerbuff = {48707},
     barcolors = {
-      cooldown    = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {212/255, 242/255, 040/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {212/255, 242/255, 040/255, 0.7}
+     },
   })
 
   -- Anti-Magic Shell
@@ -124,9 +124,9 @@ function EventHorizon:InitializeClass()
     cooldown = 48707,
     playerbuff = {205725,5},
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {212/255, 242/255, 040/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {212/255, 242/255, 040/255, 0.7}
+     },
   })
 
   -- Vampiric Blood
@@ -146,15 +146,15 @@ function EventHorizon:InitializeClass()
   })
 
   -- Frost
---[[ I was testing not having this as it's unlikely to ever be missing unless you fell asleep, feedback welcome.
+--[[ --I was testing not having this as it's unlikely to ever be missing unless you fell asleep, feedback welcome.
   -- Frost Fever
   self:newSpell({
     requiredTree = 2,
     requiredLevel = 55,
     debuff = {55095,3},
     barcolors = {
-      debuffmine  = {212/255, 242/255, 252/255, 0.7}
-    },
+     debuffmine  = {212/255, 242/255, 252/255, 0.7}
+     },
   })
 ]]
 
@@ -164,19 +164,19 @@ function EventHorizon:InitializeClass()
     requiredTalent = 2,
     playerbuff = {194878, 1.5}, -- Unhasted GCD time included as a 'tick marker' to help keep it up.
     barcolors = {
-    cooldown  = {171/255, 191/255, 181/255, 0.6},
-    debuffmine  = {212/255, 242/255, 252/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {212/255, 242/255, 252/255, 0.7}
+     },
   })
 
   -- Glacial Advance
   self:newSpell({
     requiredTree = 2,
-    requiredTalent = 21,
+    requiredTalent = 8,
     cooldown = 194913,
-    barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6}
-    },
+    barcolors    = {
+     cooldown    = {090/255, 202/255, 252/255, 0.6}
+     },
   })
 
   -- Remorseless Winter
@@ -186,9 +186,9 @@ function EventHorizon:InitializeClass()
     cooldown = 196770,
     debuff = 196770,
     barcolors = {
-    cooldown    = {151/255, 222/255, 252/255, 0.6},
-    debuffmine  = {151/255, 222/255, 252/255, 0.7}
-    },
+     cooldown    = {151/255, 222/255, 252/255, 0.6},
+     debuffmine  = {151/255, 222/255, 252/255, 0.7}
+     },
   })
 
   -- Pillar of Frost
@@ -198,9 +198,9 @@ function EventHorizon:InitializeClass()
     cooldown = 51271,
     playerbuff = 51271,
     barcolors = {
-      cooldown   = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {181/255, 252/255, 252/255, 0.6}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {181/255, 252/255, 252/255, 0.6}
+     },
   })
 
   -- Horn of Winter cooldown at half height and Razorice buff/stack count
@@ -211,9 +211,9 @@ function EventHorizon:InitializeClass()
     smallCooldown = true,
     debuff = 51714,
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {050/255, 101/255, 202/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {050/255, 101/255, 202/255, 0.7}
+     },
   })
 
   -- Obliteration
@@ -223,33 +223,9 @@ function EventHorizon:InitializeClass()
     cooldown = 207256,
     playerbuff = 207256,
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {050/255, 090/255, 111/255, 0.7}
-    },
-  })
-
-  -- Empower Rune Weapon
-  self:newSpell({
-    requiredTree = 2,
-    requiredLevel = 57,
-    requiredTalentUnselected = 8,
-    recharge = 47568,
-    barcolors = {
-      cooldown  = {070/255, 252/255, 252/255, 0.5},
-      playerbuff  = {070/255, 252/255, 252/255, 0.7}
-    },
-  })
-
-  -- Hungering Rune Weapon
-  self:newSpell({
-    requiredTree = 2,
-    requiredTalent = 8,
-    cooldown = 207127,
-    playerbuff = 207127,
-    barcolors = {
-      cooldown  = {070/255, 252/255, 252/255, 0.5},
-      playerbuff  = {070/255, 252/255, 252/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff    = {050/255, 090/255, 111/255, 0.7}
+     },
   })
 
   -- Breath of Sindragosa
@@ -259,9 +235,33 @@ function EventHorizon:InitializeClass()
     cooldown = 152279,
     playerbuff = 152279,
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {050/255, 090/255, 111/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {050/255, 090/255, 111/255, 0.7}
+     },
+  })
+
+  -- Empower Rune Weapon
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 57,
+    requiredTalentUnselected = 21,
+    recharge = 47568,
+    barcolors = {
+     cooldown    = {070/255, 252/255, 252/255, 0.5},
+     playerbuff  = {070/255, 252/255, 252/255, 0.7}
+     },
+  })
+
+  -- Hungering Rune Weapon
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 21,
+    cooldown = 207127,
+    playerbuff = 207127,
+    barcolors = {
+     cooldown    = {070/255, 252/255, 252/255, 0.5},
+     playerbuff  = {070/255, 252/255, 252/255, 0.7}
+     },
   })
 
 --[[ Not included by default as it's a five minute CD, feedback has told us this is too long to track. Uncomment if wanted.
@@ -271,8 +271,8 @@ function EventHorizon:InitializeClass()
     requiredArtifactTalent = 190778,
     cooldown = 190778,
     barcolors = {
-      cooldown  = {202/255, 161/255, 050/255, 0.7},
-    },
+     cooldown    = {202/255, 161/255, 050/255, 0.7},
+     },
   })
 ]]--
 
@@ -285,9 +285,9 @@ function EventHorizon:InitializeClass()
     cooldown = 48707,
     playerbuff = 48707,
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {212/255, 242/255, 040/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {212/255, 242/255, 040/255, 0.7}
+     },
   })
 ]]--
 
@@ -312,9 +312,9 @@ function EventHorizon:InitializeClass()
     recharge = 207317,
     refreshable = true,
     barcolors = {
-      recharge  = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {181/255, 232/255, 090/255, 0.7}
-    },
+     recharge    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {181/255, 232/255, 090/255, 0.7}
+     },
   })
 
   -- Virulent Plague & Epidemic with Ebon Fever
@@ -326,9 +326,9 @@ function EventHorizon:InitializeClass()
     recharge = 207317,
     refreshable = true,
     barcolors = {
-      recharge  = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {181/255, 232/255, 090/255, 0.7}
-    },
+     recharge    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {181/255, 232/255, 090/255, 0.7}
+     },
   })
 
   -- Festering Strike & Blighted Rune Weapon
@@ -339,9 +339,9 @@ function EventHorizon:InitializeClass()
     cooldown = 194918,
     smallCooldown = true,
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {222/255, 080/255, 242/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.8},
+     debuffmine  = {222/255, 080/255, 242/255, 0.6}
+     },
   })
 
   -- Death and Decay cooldown at half height, Sudden Doom, Necrosis and Dark Succor buff procs. The icon changes but bar colour can't (yet?).
@@ -353,9 +353,9 @@ function EventHorizon:InitializeClass()
     smallCooldown = true,
     playerbuff = {{49530}, {216974}, {101568}},
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {080/255, 010/255, 151/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {080/255, 010/255, 151/255, 0.7}
+     },
   })
 
   -- Defile cooldown at half height, Sudden Doom, Necrosis and Dark Succor buff procs. The icon changes but bar colour can't (yet?).
@@ -366,9 +366,21 @@ function EventHorizon:InitializeClass()
     smallCooldown = true,
     playerbuff = {{49530}, {216974}, {101568}},
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {080/255, 010/255, 151/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {080/255, 010/255, 151/255, 0.7}
+     },
+  })
+
+  -- Soul Reaper
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 21,
+    cooldown = 130736,
+    playerbuff = 130736,
+    barcolors = {
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {050/255, 101/255, 131/255, 0.7}
+     },
   })
 
   -- Dark Transformation
@@ -379,9 +391,9 @@ function EventHorizon:InitializeClass()
     playerbuff = 63560,
     auraunit = "pet",
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {161/255, 131/255, 080/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {161/255, 131/255, 080/255, 0.7}
+     },
   })
 
   -- Apocalypse (Artifact ability) cooldown at half height with Outbreak and Scourge of Worlds debuffs.
@@ -392,9 +404,9 @@ function EventHorizon:InitializeClass()
     smallCooldown = true,
     debuff = {{196782}, {191747}},
     barcolors = {
-      cooldown  = {202/255, 161/255, 050/255, 0.7},
-      debuffmine  = {222/255, 232/255, 090/255, 0.6}
-    },
+     cooldown    = {202/255, 161/255, 050/255, 0.7},
+     debuffmine  = {222/255, 232/255, 090/255, 0.6}
+     },
   })
 
   -- Summon Gargoyle
@@ -405,8 +417,8 @@ function EventHorizon:InitializeClass()
     cooldown = 49206,
     static = 40,
     barcolors = {
-      cooldown  = {040/255, 101/255, 121/255, 0.6},
-    },
+     cooldown    = {040/255, 101/255, 121/255, 0.6},
+     },
   })
 
   -- Dark Arbiter
@@ -416,37 +428,35 @@ function EventHorizon:InitializeClass()
     cooldown = 207349,
     static = 15,
     barcolors = {
-      cooldown  = {040/255, 101/255, 121/255, 0.6},
-    },
+     cooldown    = {040/255, 101/255, 121/255, 0.6},
+     },
   })
 
   -- Remove the --[[ and ]]-- from any of the damage reduction spells you'd like to track in Unholy spec.
 --[[
   -- Corpse Shield
-    self:newSpell({
+  self:newSpell({
     requiredTree = 3,
     requiredTalent = 14,
     cooldown = 207319,
     playerbuff = 207319,
     barcolors = {
-      recharge  = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {181/255, 232/255, 090/255, 0.7}
-    },
+     recharge    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {181/255, 232/255, 090/255, 0.7}
+     },
   })
 ]]--
 
---[[
   -- Anti-Magic Shell
   self:newSpell({
     requiredTree = 3,
     cooldown = 48707,
     playerbuff = 48707,
     barcolors = {
-      cooldown  = {171/255, 191/255, 181/255, 0.6},
-      playerbuff  = {212/255, 242/255, 040/255, 0.7}
-    },
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {212/255, 242/255, 040/255, 0.7}
+     },
   })
-]]--
 
 --[[
   -- Icebound Fortitude
@@ -456,9 +466,9 @@ function EventHorizon:InitializeClass()
     cooldown = 48792,
     playerbuff = 48792,
     barcolors = {
-      recharge  = {171/255, 191/255, 181/255, 0.6},
-      debuffmine  = {181/255, 232/255, 090/255, 0.7}
-    },
+     recharge    = {171/255, 191/255, 181/255, 0.6},
+     debuffmine  = {181/255, 232/255, 090/255, 0.7}
+     },
   })
 ]]--
 
