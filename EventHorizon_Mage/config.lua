98f549e87db085a353b65bcf84c153c659e73131
James <cluuey+github@gmail.com>
2018-07-23 04:19:41 +1000

    Mage config for 8.0.1
    All specs implemented.
    Changed past and future to 1 and 9 seconds as nothing really needs lots of forward planning, it's all very reactionary.
    Bars coloured, I've tried to choose the dominant colour from the icon but that doesn't always work when they're all so similar.
    
    Feedback appreciated.

--- EventHorizon_Mage/config.lua
+++ EventHorizon_Mage/config.lua
@@ -1,216 +1,409 @@
+EventHorizon.config.width = 290
+EventHorizon.config.height = 18
+EventHorizon.config.spacing = 0
+EventHorizon.config.staticheight = nil
+EventHorizon.config.past = -0
+
 function EventHorizon:InitializeClass()
   self.config.gcdSpellID = 118 -- Polymorph
   self.config.hastedSpellID = {118,1.7}
-  self.config.past = -1.5 -- Number of seconds to show in the past. Default = -3
-  self.config.future = 12 -- Number of seconds to show in the future. Default = 12
-  
-  -- Arcane
-    
-    -- Nether Tempest
-    self:newSpell({
-      debuff = {114923, 1},
-      requiredTalent = 16,
-      requiredTree = 1,
-     })
-  
-    -- Arcane Blast/Arcane Charges
-    self:newSpell({
-      cast = 30451,
-      requiredTree = 1,
-    })
-    
-    
-    -- Arcane Barrage/Arcane Missles/Arcane Missiles!
-    self:newSpell({
-      cooldown = 44425,
-      playerbuff = 79683,
-      channel = {5143, 5},
-      requiredTree = 1,
-    })
-    
-    -- Aluneth
-    self:newSpell({
-      cast = 224968,
-      cooldown = 224968,
-      requiredTree = 1
-    })
-    
-    -- Arcane Orb
-    self:newSpell({
-      cooldown = 153626,
-      requiredTalent = 21,
-      requiredTree = 1,
-    })
-    
-    -- Supernova
-    self:newSpell({
-      cooldown = 157980,
-      requiredTalent = 10,
-      requiredTree = 1,
-    })
-    
-    -- Presence of Mind
-    self:newSpell({
-      cooldown = 205025,
-      playerbuff = 205025,
-      requiredTree = 1
-    })
-    
-    --Arcane Power
-    self:newSpell({
-      cooldown = 12042,
-      playerbuff = 12042,
-      requiredTree = 1,
-    })
-    
-    -- Evocate
-    self:newSpell({
-      channel = {12051, 3},
-      cooldown = 12051,
-      requiredTree = 1,
-    })
-    
-    -- Mirror Image
-    self:newSpell({
-      cooldown = 55342,
-      requiredTalent = 7,
-      requiredTree = 1
-    })
-    
-    -- Rune of Power
-    self:newSpell({
-      cooldown = 116011,
-      requiredTalent = 8,
-      requiredTree = 1,
-      playerbuff = 116011
-    })
-    
-    -- Charged Up
-    self:newSpell({
-      cooldown = 205032,
-      requiredTalent = 11,
-      requiredTree = 1
-    })
-      
-  
-  -- --Fire
-  
-    -- --[[
-    -- 48107 -- Heating Up
-    -- 108853 -- Inferno Blast
-    -- 11129 -- Combustion
-    -- 11366 -- Pyroblast (3 secs)
-    -- 48108 -- Pyroblast! (proc)
-    -- 12564 -- ignite
-    -- --]]
-    
-    -- Combustion
-    self:newSpell({
-      cooldown = 190319,
-      buff = 190319,
-      requiredTree = 2,
-    })
-        
-    -- Pyroblast! and Heating Up
-    self:newSpell({
-      playerbuff = {{48107,0},{48108,0}},
-      requiredTree = 2,
-    })
-  
-    -- casts/Inferno Blast/ignite
-    self:newSpell({
-      cast = {133,11366,108853},
-      debuff = {12654,1},
-      cooldown = 108853,
-      icon = 108853,
-      requiredTree = 2,
-    })
-         
-    -- Blast Wave
-    self:newSpell({
-      cooldown = 157981,
-      requiredTalent = 10,
-      requiredTree = 2,
-    })
-    
-    -- Meteor
-    self:newSpell({
-      cooldown = 153561,
-      requiredTalent = 21,
-      requiredTree = 2,
-    })
-    
-    -- Cinderstorm
-    self:newSpell({
-      cooldown = 198292,
-      cast = 198292,
-      requiredTalent = 20,
-      requiredTree = 2,
-    })
-    
-    -- Living Bomb
-    self:newSpell({
-      debuff = {44457, 3},
-      requiredTalent = 16,
-      requiredTree = 2,
-    })
-        
-  -- -- Frost --
-    -- -- Frost Bomb
-    -- self:newSpell({
-      -- cast = 112948,
-      -- debuff = {112948, 0},
-      -- requiredTalent = 13,
-      -- requiredTree = 3
-    -- })
-  
-    -- --Pet Freeze/Water Jet/frost bolt
-    -- self:newSpell({
-      -- cast = 116,
-      -- cooldown = 135029,
-      -- channel = {135029, 1},
-      -- requiredTree = 3,
-    -- })
+  self.config.past = -1 -- Number of seconds to show in the past. Default = -1
+  self.config.future = 9 -- Number of seconds to show in the future. Default = 9
 
-    -- --Finger of Frost/Ice Orb
-    -- self:newSpell({
-      -- playerbuff = 44544,
-      -- cooldown = 84714,
-      -- requiredTree = 3,
-    -- })
-    
-    -- --Brain Freeze/icy Veins
-    -- self:newSpell({
-      -- cooldown = 12472,
-      -- playerbuff = 57761,
-      -- requiredTree = 3,
-    -- })
-  
-  
-  -- -- Mage Talents
-    -- -- Ice Nova
-    -- self:newSpell({
-      -- cooldown = 157997,
-      -- requiredTalent = 15,
-      -- requiredTree = 3,
-    -- })
+-- Arcane
 
-    -- -- Comet Storm
-    -- self:newSpell({
-      -- cooldown = 153595,
-      -- requiredTalent = 21,
-      -- requiredTree = 3,
-    -- })
+  -- Clearcasting proc, casts and Arcane Barrage CD at half height.
+  self:newSpell({
+    requiredTree = 1,
+    cast = {30451, 44424},
+    channel = {5143, 5},
+    playerbuff = 276743,
+    cooldown = 44425,
+    smallCooldown = true,
+    cast = {30451, 44424},
+    channel = {5143, 5},
+     barcolors   = {
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     },
+  })
 
-    -- -- Mirror Image
-    -- self:newSpell({
-      -- cooldown = 55342,
-      -- requiredTalent = 16,
-    -- })
-    
-    -- -- Prismatic Crystal
-    -- self:newSpell({
-      -- cooldown = 152087,
-      -- requiredTalent = 20,
-    -- })  
+  -- Nether Tempest
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = {10, 18},
+    debuff = {114923, 1},
+     barcolors   = {
+     debuffmine  = {252/255, 212/255, 252/255, 0.8},
+     },
+  })
+
+  -- Nether Tempest with Charged up CD at half height if both taken.
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = {11, 18},
+    cooldown = 205032,
+    smallCooldown = true,
+    debuff = {114923, 1},
+     barcolors   = {
+     debuffmine  = {252/255, 212/255, 252/255, 0.7},
+     cooldown    = {121/255, 141/255, 131/255, 0.9},
+     },
+  })
+
+  -- Nether Tempest with Supernova CD at half height if both taken.
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = {12, 18},
+    cooldown = 157980,
+    smallCooldown = true,
+    debuff = {114923, 1},
+     barcolors   = {
+     debuffmine  = {252/255, 212/255, 252/255, 0.7},
+     cooldown    = {121/255, 141/255, 131/255, 0.9},
+     },
+  })
+
+  -- Arcane Orb
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 21,
+    cooldown = 153626,
+     barcolors   = {
+     cooldown  = {242/255, 161/255, 252/255, 0.65},
+     },
+  })
+
+  -- Supernova
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 12,
+    requiredTalentUnselected = 18,
+    cooldown = 157980,
+  })
+
+  -- Charged Up
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 11,
+    requiredTalentUnselected = 18,
+    cooldown = 205032,
+  })
+
+  -- Rune of Power
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 9,
+    recharge = 116011,
+    playerbuff = 116011,
+    cast = 116011,
+     barcolors   = {
+     playerbuff  = {171/255, 070/255, 171/255, 0.5},
+     recharge    = {171/255, 191/255, 181/255, 0.4}
+     },
+  })
+
+-- Presence of Mind
+  self:newSpell({
+    requiredTree = 1,
+    cooldown = 205025,
+    playerbuff = 205025,
+     barcolors   = {
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+    },
+  })
+
+--Arcane Power
+  self:newSpell({
+    requiredTree = 1,
+    cooldown = 12042,
+    playerbuff = 12042,
+     barcolors   = {
+     playerbuff  = {000/255, 000/255, 232/255, 0.6},
+     },
+  })
+
+-- Evocate
+  self:newSpell({
+    requiredTree = 1,
+    channel = {12051, 3},
+    recharge = 12051,
+     barcolors   = {
+     recharge    = {202/255, 161/255, 252/255, 0.5}
+    },
+  })
+
+  -- Mirror Image
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 8,
+    cooldown = 55342,
+     barcolors   = {
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     },
+})
+
+--  Fire
+
+  -- Heating up with casts and Fireblast recharge.
+  self:newSpell({
+    requiredTree = 2,
+    cast = {133, 2948, 11366},
+    playerbuff = 48107,
+    recharge = 108853,
+     barcolors   = {
+     playerbuff  = {212/255, 111/255, 010/255, 0.7},
+     recharge    = {171/255, 191/255, 181/255, 0.4},
+     },
+  })
+
+  -- Hot Streak! and Phoenix's Flames recharge.
+  self:newSpell({
+    requiredTree = 2,
+    recharge = 257541,
+    playerbuff = 48108,
+     barcolors   = {
+     playerbuff  = {252/255, 020/255, 080/255, 0.6},
+     recharge    = {202/255, 161/255, 050/255, 0.4},
+     },
+  })
+
+-- Ignite and Dragon's Breath CD at half height.
+  self:newSpell({
+    requiredTree = 2,
+    debuff = {12654, 1},
+    cooldown = 31661,
+    smallCooldown = true,
+     barcolors   = {
+     debuffmine  = {202/255, 030/255, 000/255, 0.7},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+     },
+  })
+
+-- Living Bomb
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 18,
+    cooldown = 44457,
+    debuff = 44457,
+     barcolors   = {
+     debuffmine  = {252/255, 161/255, 040/255, 0.7},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+     },
+  })
+
+-- Meteor
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 21,
+    cooldown = 153561,
+     barcolors   = {
+     cooldown    = {222/255, 060/255, 010/255, 0.4},
+     },
+  })
+
+-- Combustion
+  self:newSpell({
+    requiredTree = 2,
+    cooldown = 190319,
+    playerbuff = 190319,
+     barcolors   = {
+     playerbuff  = {191/255, 191/255, 010/255, 0.6},
+     cooldown    = {171/255, 171/255, 171/255, 0.4},
+     },
+  })
+
+  -- Rune of Power
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 9,
+    recharge = 116011,
+    playerbuff = 116011,
+    cast = 116011,
+     barcolors   = {
+     playerbuff  = {171/255, 070/255, 171/255, 0.5},
+     recharge    = {171/255, 191/255, 181/255, 0.4}
+     },
+  })
+
+  -- Pyroclasm
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 20,
+    requiredTalentUnselected = 8,
+    playerbuff = 269650,
+  })
+
+  -- Pyroclasm with Mirror Image CD at half height, if both are taken.
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = {8 ,20},
+    playerbuff = 269650,
+    cooldown = 55342,
+    smallCooldown = true,
+     barcolors   = {
+     playerbuff  = {242/255, 161/255, 010/255, 0.5},
+     cooldown    = {171/255, 191/255, 181/255, 0.8}
+     },
+  })
+
+-- Mirror Image
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 8,
+    requiredTalentUnselected = 20,
+    cooldown = 55342,
+     barcolors   = {
+     playerbuff  = {242/255, 161/255, 010/255, 0.7},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+     },
+})
+
+-- Frost
+
+  -- Brain Freeze with spell casts and Blizzard CD at half height.
+  self:newSpell({
+    requiredTree = 3,
+    playerbuff = 190447,
+    icon = 190447,
+    cooldown = 190356,
+    smallCooldown = true,
+    cast = {116, 31687, 190356, 199786, 257537},
+     barcolors   = {
+     playerbuff  = {252/255, 232/255, 252/255, 0.5},
+     cooldown    = {030/255, 030/255, 252/255, 0.9},
+     },
+  })
+
+  -- Fingers of Frost with Frozen Orb CD at half height.
+  self:newSpell({
+    requiredTree = 3,
+    playerbuff = 112965,
+    cooldown = 84714,
+    smallCooldown = true,
+     barcolors   = {
+     playerbuff  = {030/255, 101/255, 191/255, 0.6},
+     cooldown    = {171/255, 191/255, 181/255, 0.7},
+     },
+  })
+
+  -- Bone Chilling buff with Freeze CD at half height (pet ability).
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 1,
+    playerbuff = 205766,
+    cooldown = 33395,
+    smallCooldown = true,
+  })
+
+  -- Ice Nova
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 3,
+    debuff = 157997,
+    cooldown = 157997,
+     barcolors   = {
+     cooldown    = {212/255, 222/255, 232/255, 0.4},
+    },
+  })
+
+  -- Comet Storm
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 18,
+    cooldown = 153595,
+  })
+
+  -- Ebonbolt
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 12,
+    requiredTalentUnselected = 21,
+    cooldown = 257537,
+     barcolors   = {
+     cooldown    = {040/255, 252/255, 252/255, 0.6},
+     },
+  })
+
+  -- Icicles, so we get a count for using Glacial Spike without Ebonbolt.
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 21,
+    requiredTalentUnselected = 12,
+    playerbuff = 205473,
+    icon = 205473,
+     barcolors   = {
+     playerbuff  = {242/255, 212/255, 252/255, 0.5},
+     },
+  })
+
+  -- Icicles, so we get a count for using Glacial Spike, with Ebonbolt CD at half height, if chosen.
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = {12, 21},
+    playerbuff = 205473,
+    icon = 205473,
+    cooldown = 257537,
+    smallCooldown = true,
+     barcolors   = {
+     playerbuff  = {242/255, 212/255, 252/255, 0.5},
+     cooldown    = {040/255, 252/255, 252/255, 0.6},
+     },
+  })
+
+  -- Ray of Frost
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 20,
+    debuff = {205021, 1},
+    cooldown = 205021,
+    channel = 205021,
+     barcolors   = {
+     cooldown    = {040/255, 040/255, 232/255, 0.4},
+    },
+  })
+
+  -- Rune of Power
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 9,
+    recharge = 116011,
+    playerbuff = 116011,
+    cast = 116011,
+     barcolors   = {
+     playerbuff  = {171/255, 070/255, 171/255, 0.5},
+     recharge    = {171/255, 191/255, 181/255, 0.4}
+     },
+  })
+
+  -- Mirror Image
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 8,
+    cooldown = 55342,
+     barcolors   = {
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     },
+})
+
+  -- Ice Veins
+  self:newSpell({
+    requiredTree = 3,
+    cooldown = 12472,
+    playerbuff = 12472,
+     barcolors   = {
+     playerbuff  = {030/255, 040/255, 252/255, 0.6},
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     },
+  })
+
+  -- Ice Flows
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 6,
+    recharge = 108839,
+     barcolors   = {
+     recharge    = {101/255, 202/255, 242/255, 0.6},
+     },
+  })
+
 end
