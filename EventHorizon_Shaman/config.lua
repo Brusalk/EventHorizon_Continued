d48fc89928c8f159714c9ce03be9893390da2ebf
James <cluuey+github@gmail.com>
2020-10-01 15:22:53 +1000

    Shaman 8.1 config. (#113)
    Enhancement looks and feels good.
    Elemental and Restoration untouched so far.

--- EventHorizon_Shaman/config.lua
+++ EventHorizon_Shaman/config.lua
@@ -5,7 +5,7 @@ function EventHorizon:InitializeClass()
   self.config.hastedSpellID = {2008,10} -- Ancestral Spirit
   self.config.past = -1.5 -- Number of seconds to show in the past. Default = -3
   self.config.future = 12 -- Number of seconds to show in the future. Default = 12
-
+--[[
   -- Elemental
 
   -- Lava Burst recharge, Lava Surge proc and spell casts.
@@ -14,23 +14,33 @@ function EventHorizon:InitializeClass()
     cast = {188196, 188443, 51505, 51514, 8004},
     playerbuff = 77762,
     recharge = 51505,
-    barcolors = {
+     barcolors   = {
      recharge    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {232/255, 050/255, 020/255, 0.5}
      },
   })
 
-  -- Flame Shock and Fire Elemental at half height.
+  -- Flame Shock
   self:newSpell({
     requiredTree = 1,
     requiredLevel = 3,
-    cooldown = 198067,
-    smallCooldown = true,
-    playerbuff = 205495,
+    cooldown = 188389,
     debuff = {188389, 2},
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      tick        = {222/255, 040/255, 010/255, 0.9},
+     debuffmine  = {242/255, 141/255, 010/255, 0.5}
+     },
+  })
+
+  -- Exposed Elements
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 1,
+ --   cooldown = 188389,
+    debuff = 260694,
+     barcolors   = {
+--     cooldown    = {171/255, 191/255, 181/255, 0.7},
      debuffmine  = {242/255, 141/255, 010/255, 0.5}
      },
   })
@@ -38,34 +48,34 @@ function EventHorizon:InitializeClass()
   -- Elemental Blast
   self:newSpell({
     requiredTree = 1,
-    requiredTalent = 15,
+    requiredTalent = 3,
     cast = 117014,
     cooldown = 117014,
     playerbuff = {{118522}, {173183}, {173184}}, -- 118522 crit, 173183 haste, 173184 mastery
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {171/255, 070/255, 161/255, 0.6}
      },
   })
-
+  -- 198067 Fire Elemental at half height.
   -- Icefury
   self:newSpell({
     requiredTree = 1,
-    requiredTalent = 21,
+    requiredTalent = 18,
     cast = 210714,
     cooldown = 210714,
     playerbuff = 210714,
   })
 
-  -- Stormkeeper (Artifact) CD and Power of the Maelstrom buff, CD at half height for less ambiguity.
+  -- Stormkeeper CD at half height for less ambiguity.
   self:newSpell({
     requiredTree = 1,
-    requiredArtifactTalent = 205495,
-    cast = 205495,
-    cooldown = 205495,
+    requiredTalent = 20,
+    cast = 191634,
+    cooldown = 191634,
     smallCooldown = true,
-    playerbuff = {{205495}, {191861}},
-    barcolors = {
+    playerbuff = 191634,
+     barcolors   = {
      cooldown    = {202/255, 161/255, 055/255, 0.7},
      playerbuff  = {080/255, 141/255, 191/255, 0.6}
      },
@@ -74,29 +84,29 @@ function EventHorizon:InitializeClass()
   -- Liquid Magma Totem, currently unable to track totem uptime
   self:newSpell({
     requiredTree = 1,
-    requiredTalent = 16,
+    requiredTalent = 12,
     cooldown = 192222,
   })
 
   -- Totem Mastery
   self:newSpell({
     requiredTree = 1,
-    requiredTalent = 3,
+    requiredTalent = 6,
     playerbuff = {{202192}, {210652}, {210659}, {210658}},
     icon = 210643,
-    barcolors = {
+     barcolors   = {
 --     cooldown    = {171/255, 191/255, 181/255, 0.7},
 --     playerbuff  = {232/255, 191/255, 181/255, 0.4}
      },
   })
 
-  -- Elemental Mastery
+  -- Master of the Elements new buff 260734
   self:newSpell({
     requiredTree = 1,
-    requiredTalent = 12,
+    requiredTalent = 5,
     cooldown = 16166,
     playerbuff = 16166,
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {232/255, 191/255, 181/255, 0.4}
      },
@@ -108,47 +118,22 @@ function EventHorizon:InitializeClass()
     requiredTalent = 19,
     cooldown = 114050,
     playerbuff = 114050,
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {232/255, 191/255, 181/255, 0.4}
      },
   })
-
+]]
   -- Enhancement
 
-  -- Rockbiter
+  -- Rockbiter with Hot Hand
   self:newSpell({
     requiredTree = 2,
     requiredLevel = 10,
-    requiredTalent = 1,
-    recharge = 193786,
-    barcolors = {
-     recharge    = {171/255, 191/255, 181/255, 0.3}
-     },
-  })
-
-  -- Rockbiter recharge and Hot Hand proc, if taken.
-  self:newSpell({
-    requiredTree = 2,
-    requiredLevel = 10,
-    requiredTalent = 2,
     recharge = 193786,
     playerbuff = 215785,
-    barcolors = {
+     barcolors   = {
      playerbuff  = {222/255, 080/255, 010/255, 0.6},
-     recharge    = {171/255, 191/255, 181/255, 0.3}
-     },
-  })
-
-  -- Rockbiter recharge and Landslide buff, if taken.
-  self:newSpell({
-    requiredTree = 2,
-    requiredLevel = 10,
-    requiredTalent = 3,
-    recharge = 193786,
-    playerbuff = 202004,
-    barcolors = {
-     playerbuff  = {121/255, 020/255, 030/255, 0.7},
      recharge    = {171/255, 191/255, 181/255, 0.3}
      },
   })
@@ -157,43 +142,25 @@ function EventHorizon:InitializeClass()
   self:newSpell({
     requiredTree = 2,
     requiredLevel = 12,
+    requiredTalentUnselected = 10,
     cooldown = 193796,
     playerbuff = 194084,
-    barcolors = {
+     barcolors   = {
      playerbuff  = {252/255, 141/255, 040/255, 0.6},
      cooldown    = {171/255, 191/255, 181/255, 0.6}
      },
   })
 
-  -- Frostbrand + Doomhammer CD at half height.
+  -- Searing Assault
   self:newSpell({
     requiredTree = 2,
-    requiredLevel = 38,
-    cooldown = 204945,
-    smallCooldown = true,
-    playerbuff = 196834,
-    barcolors = {
-     playerbuff  = {080/255, 111/255, 151/255, 0.5},
-     cooldown    = {202/255, 161/255, 050/255, 0.8},
-     },
-  })
-
-  -- Overcharge
-  self:newSpell({
-    requiredTree = 2,
-    requiredTalent = 14,
-    cooldown = 187837,
-  })
-
-  -- Windsong
-  self:newSpell({
-    requiredTree = 2,
-    requiredTalent = 1,
-    cooldown = 201898,
-    playerbuff = 201898,
-    barcolors = {
-     playerbuff  = {151/255, 242/255, 242/255, 0.6},
-     cooldown    = {171/255, 191/255, 181/255, 0.6}
+    requiredTalent= 10,
+    cooldown = 193796,
+    debuff = {268429, 2},
+     barcolors   = {
+     cooldown    = {171/255, 191/255, 181/255, 0.7},
+     tick        = {222/255, 040/255, 010/255, 0.9},
+     debuffmine  = {252/255, 141/255, 040/255, 0.6}
      },
   })
 
@@ -203,19 +170,37 @@ function EventHorizon:InitializeClass()
     requiredLevel = 20,
     cooldown = 17364,
     playerbuff = 201846,
-    barcolors = {
+     barcolors   = {
      playerbuff  = {010/255, 010/255, 171/255, 0.6},
      cooldown    = {171/255, 191/255, 181/255, 0.6}
      },
   })
 
+  -- Frostbrand - only with Hailstorm talented.
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 38,
+    requiredTalent = 11,
+    playerbuff = 196834,
+     barcolors   = {
+     playerbuff  = {080/255, 111/255, 151/255, 0.5},
+     },
+  })
+
+  -- Overcharge
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 12,
+    cooldown = 187837,
+  })
+
   -- Earthen Spike
   self:newSpell({
     requiredTree = 2,
-    requiredTalent = 21,
+    requiredTalent = 20,
     cooldown = 188089,
     debuff = 188089,
-    barcolors = {
+     barcolors   = {
      cooldown    = {111/255, 090/255, 050/255, 0.4},
      debuffmine  = {101/255, 080/255, 040/255, 0.6}
      },
@@ -243,7 +228,7 @@ function EventHorizon:InitializeClass()
     cooldown = 51533,
     smallCooldown = true,
     playerbuff = 197211,
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {212/255, 212/255, 242/255, 0.4}
      },
@@ -252,15 +237,29 @@ function EventHorizon:InitializeClass()
   -- Ascendance
   self:newSpell({
     requiredTree = 2,
-    requiredTalent = 19,
+    requiredTalent = 21,
     cooldown = 114051,
     playerbuff = 114051,
-    barcolors = {
-     cooldown    = {171/255, 191/255, 181/255, 0.7},
-     playerbuff  = {232/255, 191/255, 181/255, 0.4}
+     barcolors   = {
+     cooldown    = {222/255, 212/255, 191/255, 0.3},
+     playerbuff  = {252/255, 191/255, 181/255, 0.6}
      },
   })
 
+--[[ seems to have been removed but the spell still exists in the Db, unlike others which were removed, so I'm leaving it here for future changes.
+  -- Windsong
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 1,
+    cooldown = 201898,
+    playerbuff = 201898,
+     barcolors   = {
+     playerbuff  = {151/255, 242/255, 242/255, 0.6},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+     },
+  })
+]]--
+[[
   -- Restoration
 
   -- Lava Burst recharge, Lava Surge proc and spell casts.
@@ -269,7 +268,7 @@ function EventHorizon:InitializeClass()
     cast = {403, 421, 1064, 8004, 51505, 51514, 73920, 77472, 207778},
     playerbuff = 77762,
     recharge = 51505,
-    barcolors = {
+     barcolors   = {
      recharge    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {232/255, 050/255, 020/255, 0.5}
      },
@@ -281,7 +280,7 @@ function EventHorizon:InitializeClass()
     requiredLevel = 3,
     cooldown = 188838,
     debuff = {188838, 3},
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      tick        = {222/255, 040/255, 010/255, 0.9},
      debuffmine  = {242/255, 141/255, 010/255, 0.5}
@@ -295,7 +294,7 @@ function EventHorizon:InitializeClass()
     recharge = 61295,
     playerbuff = {61295, 3},
     auraunit = 'mouseover' or 'target',
-    barcolors = {
+     barcolors   = {
      recharge    = {171/255, 191/255, 181/255, 0.7},
      tick        = {040/255, 181/255, 020/255, 0.9},
      playerbuff  = {030/255, 090/255, 090/255, 0.6}
@@ -307,7 +306,7 @@ function EventHorizon:InitializeClass()
     requiredTree = 3,
     requiredLevel = 40,
     cooldown = 73920,
-    barcolors = {
+     barcolors   = {
      cooldown    = {010/255, 101/255, 232/255, 0.7},
      playerbuff  = {080/255, 181/255, 131/255, 0.5}
      },
@@ -319,7 +318,7 @@ function EventHorizon:InitializeClass()
     requiredTalent = 2,
     cooldown = 73685,
     playerbuff = 73685,
-    barcolors = {
+     barcolors   = {
      cooldown    = {090/255, 191/255, 191/255, 0.7},
      playerbuff  = {080/255, 181/255, 131/255, 0.5}
      },
@@ -331,7 +330,7 @@ function EventHorizon:InitializeClass()
     requiredLevel = 14,
     recharge = 5394,
 --    totem = 5394,
-    barcolors = {
+     barcolors   = {
      recharge    = {080/255, 161/255, 202/255, 0.7},
      playerbuff  = {080/255, 161/255, 202/255, 0.4}
      },
@@ -344,7 +343,7 @@ function EventHorizon:InitializeClass()
     playerbuff = 53390,
     cooldown = 207778,
     smallCooldown = true,
-    barcolors = {
+     barcolors   = {
      cooldown    = {202/255, 161/255, 055/255, 0.7},
      },
   })
@@ -357,7 +356,7 @@ function EventHorizon:InitializeClass()
     playerbuff = 201633,
     auraunit = 'mouseover' or 'target',
 --    totem = 198838,
-    barcolors = {
+     barcolors   = {
      cooldown    = {040/255, 090/255, 090/255, 0.6},
      playerbuff  = {232/255, 090/255, 242/255, 0.7}
      },
@@ -369,7 +368,7 @@ function EventHorizon:InitializeClass()
     requiredTalent = 11,
     cooldown = 108281,
     playerbuff = 108281,
-    barcolors = {
+     barcolors   = {
      cooldown    = {16/255, 252/255, 252/255, 0.6},
 --     playerbuff  = {232/255, 191/255, 181/255, 0.4}
      },
@@ -381,7 +380,7 @@ function EventHorizon:InitializeClass()
     requiredLevel = 80,
     cooldown = 108280,
 --    totem = 108280,
-    barcolors = {
+     barcolors   = {
      cooldown    = {151/255, 242/255, 222/255, 0.7},
      playerbuff  = {151/255, 242/255, 222/255, 0.4}
      },
@@ -393,12 +392,12 @@ function EventHorizon:InitializeClass()
     requiredTalent = 19,
     cooldown = 114052,
     playerbuff = 114052,
-    barcolors = {
+     barcolors   = {
      cooldown    = {171/255, 191/255, 181/255, 0.7},
      playerbuff  = {232/255, 191/255, 181/255, 0.4}
      },
   })
-
+]]
 -- Utility spells or long cooldowns, hidden by default as there are already many bars for this spec. Remove the comment "--[[ ]]--" block lines to show the ones you'd like.
 --[[
   -- Spiritwalker's Grace
@@ -419,7 +418,7 @@ function EventHorizon:InitializeClass()
     playerbuff = 207498,
     auraunit = 'mouseover' or 'target',
 --    totem = 207399,
-    barcolors = {
+     barcolors   = {
      playerbuff  = {232/255, 010/255, 010/255, 0.7}
      },
   })
